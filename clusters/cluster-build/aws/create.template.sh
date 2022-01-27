0448960d1bd40cda23561d3606696a0ce78560a3
Cong Nguyen <rampadc@users.noreply.github.com>
2022-01-27 20:52:54 +1100

    Reverting changes to script

--- clusters/cluster-build/aws/create.template.sh
+++ clusters/cluster-build/aws/create.template.sh
@@ -6,8 +6,5 @@ export SSH_PRIV="$(cat id_ed25519)"
 export SSH_PUB="$(cat id_ed25519.pub)"
 export PULL_SECRET=$(cat pullsecret)
 export CLUSTER_NAME="aws1"
-export REGION="ap-southeast-1"
-export POD_CIDR="10.128.0.0/14"
-export SVC_CIDR="172.30.0.0/16"
 
 ./make_secret-aws.sh
